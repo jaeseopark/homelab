dc8a088ed660062b77b0576b4d2ac5c6e55e67af
jaeseopark <jaeseopark@crispbox.lan>
2021-10-19 23:44:18 -0600

    updated script

--- rpi/prep_image.sh
+++ rpi/prep_image.sh
@@ -1,18 +1,19 @@
-#/bin/bash -e
+#!/bin/bash -ex
 
 IMG_URL="https://github.com/timebertt/pi-cloud-init/releases/download/2021-05-10/2021-05-10-raspios-buster-armhf-lite-cloud-init.zip "
 
-if [[ $EUID -ne 0 ]]; then
+if [[ ${EUID} -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 fi
 
-function download_image() {
+download_image() {
+    rm -f *.zip *.img
     apt-get install unzip # if not already installed
     wget ${IMG_URL} && unzip *.zip
 }
 
-function mount_image() {
+mount_image() {
     if [ -z $(which fdisk) ]; then
         alias fdisk="/sbin/fdisk"
     fi
@@ -24,13 +25,14 @@ function mount_image() {
     mount -o loop,offset=${offset} *.img /mnt/tmp
 }
 
-function copy_files_to_mounted_volumn() {
+copy_files_to_mounted_volume() {
     cp init/* /mnt/tmp/
     umount /mnt/tmp
 }
 
-download_image()
-mount_image()
-copy_files_to_mounted_volumn()
+download_image
+mount_image
+copy_files_to_mounted_volume
 
+echo "------------------------------------------"
 echo "Image is ready: $(ls *.img)"
